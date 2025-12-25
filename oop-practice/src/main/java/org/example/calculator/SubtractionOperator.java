6f64be9bca01f50ff70eb968b113ea35d9b1ddbc
Minji6 <105301353+Minji6@users.noreply.github.com>
2025-12-25 16:59:23 +0900

    refactor: 연산자 클래스의 매개변수 타입을 PositiveNumber로 변경
    - 원시 타입 int 대신 PositiveNumber 객체 사용
    - toInt() 메서드로 정수값 변환 후 연산 수행

--- oop-practice/src/main/java/org/example/calculator/SubtractionOperator.java
+++ oop-practice/src/main/java/org/example/calculator/SubtractionOperator.java
@@ -7,7 +7,7 @@ public class SubtractionOperator implements NewArithmeticOperator {
     }
 
     @Override
-    public int calculate(int operand1, int oeprand2){
-        return operand1 - oeprand2;
+    public int calculate(PositiveNumber operand1, PositiveNumber oeprand2){
+        return operand1.toInt() - oeprand2.toInt();
     }
 }
